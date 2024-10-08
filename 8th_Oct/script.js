59f79550ead1f31d74d52c79e2f22e9deccf8d06
SharatRokkam <rokkamsharath1@gmail.com>
2024-10-08 19:11:00 +0530

    js basics

--- 8th_Oct/script.js
+++ 8th_Oct/script.js
@@ -1,7 +1,74 @@
-// console.log(4 + 5);
+// var age = 25;
+// let student = "John";
+// const isStudent = true;
 
-// alert("Javascript Session");
+// console.log(age);
+// console.log(isStudent);
+// console.log(student);
+// ctrl + /
 
-// document.getElementById("output").innerText = "Hope you learned something new";
+// const studentName = ["Apoorva", 1, true, {}, []]
 
-// prompt("enter your name");
+// const obj1 = {
+//   id: 1,
+//   studentName: "John",
+//   age: 22,
+// };
+
+// let a = 10;
+// let b = 5;
+
+// // Arithmetic
+// let sum = a + b; // 15
+
+// let difference = a - b; // 5
+
+// // Comparison
+// let isEqual = a == b; // false
+// let isGreater = a > b; // true
+
+// console.log(sum, difference, isEqual, isGreater);
+
+// function addNumbers(x, y) {
+//   return x + y;
+// }
+
+// console.log(addNumbers(3, 7)); // 10
+// console.log(addNumbers(4, 8)); // 10
+// console.log(addNumbers(7, 3)); // 10
+// console.log(addNumbers(8, 7)); // 10
+// console.log(addNumbers(10, 7)); // 10
+
+// DOM - Document Object Model
+
+// const newDiv = document.createElement("div");
+// newDiv.textContent = "I am a new div!";
+
+// // adopting
+// document.body.appendChild(newDiv);
+
+// let count = 0;
+
+// document.getElementById("ClickBtn").addEventListener("click", function () {
+//   // count = count + 1;
+//   count++;
+//   document.getElementById("count").innerText = count;
+// });
+
+// conditional statement
+
+let isWhite = true;
+
+let button = document.querySelector("#btn");
+
+button.addEventListener("click", function () {
+  if (isWhite) {
+    document.body.style.backgroundColor = "black";
+    button.textContent = "Change to White";
+  } else {
+    document.body.style.backgroundColor = "white";
+    button.textContent = "Change to Black";
+  }
+
+  isWhite = !isWhite;
+});
